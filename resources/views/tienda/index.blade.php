ed697200938b69acfec81263ec00954623dfd6e8
vndrd <ivangddzzz@gmail.com>
2020-05-04 21:09:19 -0400

    laravel mix

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -1,6 +1,9 @@
 @extends('plantilla.plantilla')
-
 @section('titulo','Shop')
+@section('estilos')
+    <link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/main_styles.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('asset/styles/responsive.css') }}">
+@endsection
 
 @section('contenido')
 
